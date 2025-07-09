dcd83d4aadfedc18610a73567dfcd926910da8a8
Iksita-Bala <85560226+Iksita-B@users.noreply.github.com>
2025-07-09 16:34:41 +0530

    columns update

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ pokemonList }) {
       <div className="flex justify-center">
         <SearchBar value={search} onChange={setSearch} />
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-6">
         {filtered.map((pokemon, index) => (
           <PokemonCard key={index} name={pokemon.name} url={pokemon.url} />
         ))}
